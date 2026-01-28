566b4f62682ced85e93dce897a12be50e03e57a6
Timothy Brooke <brooketimothy494@gmail.com>
2026-01-28 14:25:01 +0100

    Extend Note model to store camera image path

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -2,6 +2,7 @@ class Note {
   final String id;
   final String title;
   final String body;
+  final String? imagePath; // NEW
   final DateTime createdAt;
 
   Note({
@@ -9,12 +10,14 @@ class Note {
     required this.title,
     required this.body,
     required this.createdAt,
+    this.imagePath, // NEW
   });
 
   Map<String, dynamic> toMap() => {
         "id": id,
         "title": title,
         "body": body,
+        "imagePath": imagePath, // NEW
         "createdAt": createdAt.toIso8601String(),
       };
 
@@ -22,6 +25,7 @@ class Note {
         id: map["id"] as String,
         title: map["title"] as String,
         body: map["body"] as String,
+        imagePath: map["imagePath"] as String?, // NEW
         createdAt: DateTime.parse(map["createdAt"] as String),
       );
 }
